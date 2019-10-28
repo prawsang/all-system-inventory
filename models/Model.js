d9d4e6aac358a631a3b8d1335242234a3e7515f4
Prawsang <prawcha.p@gmail.com>
2019-10-28 15:33:19 +0700

    (back): edit model model

--- models/Model.js
+++ models/Model.js
@@ -49,8 +49,21 @@ const Model = db.define("model", {
 	freezeTableName: "model"
 });
 
+// Filtering
+Model.filter = data => {
+	const { type } = data;
+	let typeFilter = type ? `"model"."is_product_type_name" = :type` : null;
+	return typeFilter
+};
+
 Model.getColumns = `"model"."model_code" AS "model_code",
-	"model"."name" AS "model_name"`;
+	"model"."name" AS "model_name",
+	"model"."weight",
+	"model"."width",
+	"model"."height",
+	"model"."depth",
+	"model"."from_supplier_code",
+	"model"."is_product_type_name"`;
 	
 Model.belongsTo(ProductType, {
 	foreignKey: "is_product_type_name",
